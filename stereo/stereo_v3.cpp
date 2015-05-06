8715d465486aead461bb6e06712752d3ec54e4ac
Rasmus <rdiederichse@uos.de>
2015-05-06 20:07:34 +0200

    exported some stuff to header file; changed invocation syntax; most options now inputtable

--- stereo/stereo_v3.cpp
+++ stereo/stereo_v3.cpp
@@ -1,45 +1,160 @@
-/**
- * @file OCV3 compliant version of stereo_v2.cpp
- * @author Rasmus Diederichsen
- */
-#include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d/nonfree.hpp>
-#include <iostream>
 #include <cmath>
+#include "stereo_v3.hpp"
 
-using namespace cv;
-using namespace std;
 
-void drawEpilines(const Mat& image_points, int whichImage, Mat& F, Mat& canvas);
-double computeReprojectionError(vector<Point2f>& img1pts, vector<Point2f>& img2pts, Mat& inlier_mask, const Mat& F);
-
-int main(int argc, const char *argv[])
+static ostream& operator<<(ostream& os, const DetectorData& d)
 {
-   if (argc != 4) 
+   os << boolalpha;
+   os << "Detector data: " << "\n"
+      << "\tminHessian: " << d.minHessian << "\n"
+      << "\tnOctaves: " << d.nOctaves << "\n"
+      << "\tnOctaveLayersSurf: " << d.nOctaveLayersSurf << "\n"
+      << "\tnOctaveLayersAkaze: " << d.nOctaveLayersAkaze << "\n"
+      << "\textended: " << d.extended << "\n"
+      << "\tupright: " << d.upright << "\n"
+      << "\tDescriptor size: " << d.descriptor_size << "\n"
+      << "\tDescriptor channels: " << d.descriptor_channels << "\n"
+      << "\tthreshold: " << d.threshold << "\n";
+}
+static ostream& operator<<(ostream& os, const CommandArgs& d)
+{
+   os << boolalpha;
+   os << "Left image: " << d.left_image_name << "\n"
+      << "Right image: " << d.right_image_name << "\n"
+      << "Calib file: " << d.calib_file_name << "\n"
+      << "Resize factor: " << d.resize_factor << "\n"
+      << "Undistort: " << d.undistort << "\n"
+      << "Epilines: " << d.epilines << "\n"
+      << "Detector type: " << (d.detector == DETECTOR_SURF ? "SURF" : "KAZE") << "\n"
+      << d.detector_data;
+}
+
+static CommandArgs parse_args(int& argc, char* const* argv)
+{
+   CommandArgs args;
+   for (int i = 1; i < argc; i++) 
    {
-      cout << "Usage: " << argv[0] << " left_img right_img calib_file.xml" << endl;
+      if (0 == strcmp(argv[i], "--left") && argc > i + 1) 
+      {
+         args.left_image_name = argv[++i];
+      }
+      else if (0 == strcmp(argv[i], "--right") && argc > i + 1) 
+      {
+         args.right_image_name = argv[++i];
+      }
+      else if (0 == strcmp(argv[i], "--calib") && argc > i + 1) 
+      {
+         args.calib_file_name = argv[++i];
+      }
+      else if (0 == strcmp(argv[i], "--resize") && argc > i + 1) 
+      {
+         args.resize_factor = atoi(argv[++i]);
+      }
+      else if (0 == strcmp(argv[i], "--no-undistort")) 
+      {
+         args.undistort = false;
+      }
+      else if (0 == strcmp(argv[i], "--epilines")) 
+      {
+         args.epilines = true;
+      }
+      else if (0 == strcmp(argv[i], "--detector") && argc > i + 1) 
+      { 
+         if (0 == strcmp(argv[i+1], "KAZE")) 
+         {
+            args.detector = DETECTOR_KAZE;
+         }
+         else if (0 == strcmp(argv[i+1], "SURF")) 
+         {
+            args.detector = DETECTOR_SURF;
+         }
+         else cout << "Unknonw detector " << argv[i+1] << endl;
+         i++;
+      }
+      else if (0 == strcmp(argv[i], "--hessianT") && argc > i + 1)
+      {
+         args.detector_data.minHessian = atoi(argv[++i]);
+      }
+      else if (0 == strcmp(argv[i], "--octaves") && argc > i + 1)
+      {
+         args.detector_data.nOctaves = atoi(argv[++i]);
+      }
+      else if (0 == strcmp(argv[i], "--octave-layers") && argc > i + 1)
+      {
+         args.detector_data.nOctaveLayersSurf = args.detector_data.nOctaveLayersAkaze = atoi(argv[++i]);
+      }
+      else if (0 == strcmp(argv[i], "--no-extended"))
+      {
+         args.detector_data.extended = false;
+      }
+      else if (0 == strcmp(argv[i], "--upright"))
+      {
+         args.detector_data.upright = true;
+      }
+      else if (0 == strcmp(argv[i], "--descriptor-size") && argc > i + 1)
+      {
+         args.detector_data.descriptor_size = atoi(argv[++i]);
+      }
+      else if (0 == strcmp(argv[i], "--descriptor-channels") && argc > i + 1)
+      {
+         args.detector_data.descriptor_channels = atoi(argv[++i]);
+      }
+      else if (0 == strcmp(argv[i], "--threshold") && (argc > i + 1))
+      {
+         args.detector_data.threshold = static_cast<float>(atof(argv[++i]));
+      }
+      else cout << "Useless parameter: " << argv[i++] << endl;
+   }
+   return args;
+}
+
+bool check_args(const CommandArgs& args)
+{
+   if (0 == strcmp(args.left_image_name,"n/a") || 0 == strcmp(args.left_image_name, "n/a") || 0 == strcmp(args.left_image_name, "n/a")) 
+      return false;
+   else return true;
+}
+
+int main(int argc, char *argv[])
+{
+   CommandArgs args = parse_args(argc, argv);
+   if (!check_args(args)) 
+   {
+      cout << "Usage: " << argv[0] << " --left IMG --right IMG2 --calib CALIB_FILE "
+         "[--resize n] [--detector (KAZE|SURF) [--hessianT n] [--octaves n] [--octave-layers = n] [--no-extend] [--upright] [--descriptor-size n] [--descriptor-channels {1,2,3}] [--threshold n]] [--epilines] [--undistort] [--no-undistort]" << endl;
       return -1;
    }
-   Mat img_1 = imread(argv[1], IMREAD_GRAYSCALE);
-   Mat img_2 = imread(argv[2], IMREAD_GRAYSCALE);
+   cout << args << endl;
+   Mat img_1 = imread(args.left_image_name, IMREAD_GRAYSCALE);
+   Mat img_2 = imread(args.right_image_name, IMREAD_GRAYSCALE);
 
-   FileStorage fs(argv[3], FileStorage::READ);
+   FileStorage fs(args.calib_file_name, FileStorage::READ);
    if (fs.isOpened())
    {
       Mat camera_matrix, dist_coefficients, img_1_undist, img_2_undist;
       fs["Camera_Matrix"] >> camera_matrix;
       fs["Distortion_Coefficients"] >> dist_coefficients;
       fs.release();
-      /* img_1_undist = img_1; */
-      /* img_2_undist = img_2; */
-      undistort(img_1, img_1_undist, camera_matrix, dist_coefficients); // remove camera imperfections
-      undistort(img_2, img_2_undist, camera_matrix, dist_coefficients);
-      resize(img_1_undist, img_1_undist, Size(img_1_undist.cols / 5, img_1_undist.rows / 5)); // make smaller for performance and displayablity
-      resize(img_2_undist, img_2_undist, Size(img_2_undist.cols / 5, img_2_undist.rows / 5));
-
-      // scale matrix down according to changed resolution
-      camera_matrix = camera_matrix / 5.0;
-      camera_matrix.at<double>(2,2) = 1;
+      if (args.undistort) 
+      {
+         undistort(img_1, img_1_undist, camera_matrix, dist_coefficients); // remove camera imperfections
+         undistort(img_2, img_2_undist, camera_matrix, dist_coefficients);
+      } else
+      {
+         img_1_undist = img_1;
+         img_2_undist = img_2;
+      }
+      if (args.resize_factor > 1) 
+      {
+         resize(img_1_undist, img_1_undist, Size(img_1_undist.cols / args.resize_factor, 
+                  img_1_undist.rows / args.resize_factor)); // make smaller for performance and displayablity
+         resize(img_2_undist, img_2_undist, Size(img_2_undist.cols / args.resize_factor,
+                  img_2_undist.rows / args.resize_factor));
+         // scale matrix down according to changed resolution
+         camera_matrix = camera_matrix / 5.0;
+         camera_matrix.at<double>(2,2) = 1;
+      }
 
       if(!img_1_undist.data || !img_2_undist.data) 
       {
@@ -48,23 +163,35 @@ int main(int argc, const char *argv[])
       }
 
       // Feature detection + extraction
-      int minHessian = 800; // changing this dramatically affects the result, set lower if you want to see nothing 
       vector<KeyPoint> KeyPoints_1, KeyPoints_2;
       Mat descriptors_1, descriptors_2;
 
-      Ptr<xfeatures2d::SURF> surf = xfeatures2d::SURF::create(minHessian, 4, 2, true, false);
-      surf->detectAndCompute(img_1_undist, noArray(), KeyPoints_1, descriptors_1);
-      surf->detectAndCompute(img_2_undist, noArray(), KeyPoints_2, descriptors_2);
+      Ptr<Feature2D> feat_detector;
+      if (args.detector == DETECTOR_KAZE) 
+      {
+      feat_detector = AKAZE::create(args.detector_data.upright ? AKAZE::DESCRIPTOR_KAZE_UPRIGHT : AKAZE::DESCRIPTOR_KAZE, 
+            args.detector_data.descriptor_size,
+            args.detector_data.descriptor_channels,
+            args.detector_data.threshold,
+            args.detector_data.nOctaves,
+            args.detector_data.nOctaveLayersAkaze);
+         
+      } else {
+         feat_detector = xfeatures2d::SURF::create(args.detector_data.minHessian, 
+               args.detector_data.nOctaves, args.detector_data.nOctaveLayersAkaze, args.detector_data.extended, args.detector_data.upright);
+      }
+      feat_detector->detectAndCompute(img_1_undist, noArray(), KeyPoints_1, descriptors_1);
+      feat_detector->detectAndCompute(img_2_undist, noArray(), KeyPoints_2, descriptors_2);
 
       // Find correspondences
-      BFMatcher matcher(NORM_L1, true);
+      BFMatcher matcher(NORM_L2, true);
       vector<DMatch> matches;
       matcher.match(descriptors_1, descriptors_2, matches);
 
 
       // Convert correspondences to vectors
       vector<Point2f>imgpts1,imgpts2;
-      for( unsigned int i = 0; i<matches.size(); i++ ) 
+      for(unsigned int i = 0; i<matches.size(); i++) 
       {
          imgpts1.push_back(KeyPoints_1[matches[i].queryIdx].pt); 
          imgpts2.push_back(KeyPoints_2[matches[i].trainIdx].pt); 
@@ -77,9 +204,10 @@ int main(int argc, const char *argv[])
       /* undistortPoints(imgpts1, imgpts1_undist, camera_matrix, dist_coefficients); */
       /* undistortPoints(imgpts2, imgpts2_undist, camera_matrix, dist_coefficients); */
       Mat E = findEssentialMat(imgpts1_undist, imgpts2_undist, 1, Point2d(0,0), RANSAC, 0.99, 9, mask);
+      correctMatches(E, imgpts1_undist, imgpts2_undist, imgpts1_undist, imgpts2_undist);
 
       Mat R, t; // rotation and translation
-      recoverPose(E, imgpts1_undist, imgpts2_undist, R, t);
+      cout << "Pose recovery inliers: " << recoverPose(E, imgpts1_undist, imgpts2_undist, R, t) << endl;
 
       double theta_x, theta_y, theta_z;
       theta_x = atan2(R.at<double>(2,1),  R.at<double>(2,2));
@@ -94,23 +222,30 @@ int main(int argc, const char *argv[])
 
       double err = computeReprojectionError(imgpts1_undist, imgpts2_undist, mask, E);
       cout << "average reprojection err = " <<  err << endl;
-      /* drawEpilines(Mat(imgpts1), 1, F, img_2_undist); */
-      /* drawEpilines(Mat(imgpts2), 2, F, img_1_undist); */
+      if (args.epilines)
+      {
+         drawEpilines(Mat(imgpts1_undist), 1, E, img_2_undist);
+         drawEpilines(Mat(imgpts2_undist), 2, E, img_1_undist);
+      }
 
       Mat img_matches; // side-by-side comparison
       drawMatches(img_1_undist, KeyPoints_1, img_2_undist, KeyPoints_2, // draw only inliers given by mask
             matches, img_matches, Scalar::all(-1), Scalar::all(-1), mask);
       // display
-      namedWindow( "Matches", CV_WINDOW_NORMAL );
-      imshow("Matches", img_matches );
+      namedWindow("Matches", CV_WINDOW_NORMAL);
+      imshow("Matches", img_matches);
       waitKey(0);
 
       return 0;
+   } else
+   {
+      cout << "Could not read file " << args.calib_file_name << endl;
+      return -1;
    }
 }
 
 
-void drawEpilines(const Mat& image_points, int whichImage, Mat& F, Mat& canvas)
+static void drawEpilines(const Mat& image_points, int whichImage, Mat& F, Mat& canvas)
 {
    // draw the left points corresponding epipolar
    // lines in right image
@@ -132,7 +267,7 @@ void drawEpilines(const Mat& image_points, int whichImage, Mat& F, Mat& canvas)
    }
 }
 
-double computeReprojectionError(vector<Point2f>& imgpts1, vector<Point2f>& imgpts2, Mat& inlier_mask, const Mat& F)
+static double computeReprojectionError(vector<Point2f>& imgpts1, vector<Point2f>& imgpts2, Mat& inlier_mask, const Mat& F)
 {
    double err = 0;
    vector<Vec3f> lines[2];
---
+++ stereo/stereo_v3.hpp
@@ -0,0 +1,44 @@
+/**
+ * @file OCV3 compliant version of stereo_v2.cpp
+ * @author Rasmus Diederichsen
+ */
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+typedef enum { DETECTOR_SURF, DETECTOR_KAZE } detector_type;
+
+typedef struct 
+{
+   double minHessian = 800; // changing this dramatically affects the result, set lower if you want to see nothing 
+   int nOctaves = 4;
+   int nOctaveLayersSurf = 2;
+   int nOctaveLayersAkaze = 4;
+   bool extended = true;
+   bool upright = false;
+   int descriptor_size = 0;
+   int descriptor_channels = 3;
+   float threshold = .001;
+
+} DetectorData;
+
+typedef struct 
+{
+   char* left_image_name = "n/a";
+   char* right_image_name = "n/a";
+   char* calib_file_name = "n/a";
+   int resize_factor = 1;
+   bool undistort = true;
+   bool epilines = false;
+   detector_type detector = DETECTOR_KAZE;
+   DetectorData detector_data;
+} CommandArgs;
+
+static void drawEpilines(const Mat& image_points, int whichImage, Mat& F, Mat& canvas);
+static double computeReprojectionError(vector<Point2f>& img1pts, vector<Point2f>& img2pts, Mat& inlier_mask, const Mat& F);
+static ostream& operator<<(ostream& os, const DetectorData& d);
+static ostream& operator<<(ostream& os, const CommandArgs& d);
+static CommandArgs parse_args(int& argc, char* const* argv);
+
