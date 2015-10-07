bc3e04104665a3ca3567f40b1dd9f78e3cd8fd89
themightyoarfish <rdiederichse@uos.de>
2015-10-07 17:26:03 +0200

    Points can be drawn

--- label_imgs/DualImageWindow.cpp
+++ label_imgs/DualImageWindow.cpp
@@ -1,10 +1,14 @@
 #include "DualImageWindow.hpp"
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+static const int ESC = 27; // ascii of escape key
 
 namespace imagelabeling 
 {
    using namespace cv;
-   DualImageWindow::DualImageWindow(Mat left_img, Mat right_img, string window_name)
+   DualImageWindow::DualImageWindow(const Mat left_img, const Mat right_img, const string window_name)
    {
       this->left_img = left_img;
       this->right_img = right_img;
@@ -14,19 +18,47 @@ namespace imagelabeling
    {
       destroyWindow(window_name);
    }
+   void DualImageWindow::refresh()
+   {
+      imshow(window_name, combined_imgs);
+   }
    void DualImageWindow::show()
    {
 
-      Mat combined_imgs(max(left_img.rows, right_img.rows), left_img.cols + right_img.cols, left_img.type());
+      combined_imgs = Mat(max(left_img.rows, right_img.rows), left_img.cols + right_img.cols, left_img.type());
       Mat left_roi(combined_imgs, Rect(0, 0, left_img.cols, left_img.rows));
       Mat right_roi(combined_imgs, Rect(left_img.cols, 0, right_img.cols, right_img.rows));
       left_img.copyTo(left_roi);
       right_img.copyTo(right_roi);
 
       namedWindow(window_name, WINDOW_NORMAL);
-      imshow(window_name, combined_imgs);
-      waitKey(0);
+      refresh();
+      setMouseCallback(window_name, mouseCallback, this);
+      while(waitKey(0) != ESC);
 
+   }
+   void DualImageWindow::mouseCallback(int event, int x, int y, int flags, void* data)
+   {
+
+      if (data) 
+      {
+         DualImageWindow* self = static_cast<DualImageWindow*>(data);
+         switch (event)
+         {
+            case EVENT_LBUTTONDOWN: 
+               if (flags & EVENT_FLAG_SHIFTKEY) // use the KEY as a bitmask. This will break if they ever change the enum values.
+               {
+                  circle(self->combined_imgs, Point2i(x,y), 8, Scalar(0,0,255), 2);
+                  std::cout << "Circle at (" << x << "," << y << ")" << std::endl;
+                  self->refresh();
+               }
+               break;
+            default:
+               break;
+         }
+
+      }
+      /* std::cout << "Event: " << event << "\n" << "x,y: " << x << "," << y << "\n" << "Flags: " << flags << std::endl; */
    }
 } /* namespace imagelabeling */
 
--- label_imgs/DualImageWindow.hpp
+++ label_imgs/DualImageWindow.hpp
@@ -1,18 +1,25 @@
 #include <opencv2/core.hpp>
+#include <map>
 using std::string;
+using std::map;
 using cv::Mat;
+using cv::Point2i;
 namespace imagelabeling 
 {
    class DualImageWindow 
    {
       public:
-         explicit DualImageWindow (Mat left_img, Mat right_img, string window_name = "Label Images");
+         explicit DualImageWindow (const Mat left_img, const Mat right_img, 
+               const string window_name = "Label Images");
          void show();
+         static void mouseCallback(int event, int x, int y, int flags, void* data);
          virtual ~DualImageWindow();
 
       private:
-         Mat left_img, right_img;
+         void refresh();
+         Mat left_img, right_img, combined_imgs;
          string window_name;
+         map<Point2i,Point2i> correspondences;
    };
 
 } /* namespace imagelabeling */
