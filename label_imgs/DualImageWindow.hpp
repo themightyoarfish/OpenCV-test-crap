09e13aec03d6d9801d89a63ff76ed456c418378b
themightyoarfish <rdiederichse@uos.de>
2015-10-25 13:42:16 +0100

    Initial correspondences can now be passed to the window

--- label_imgs/DualImageWindow.hpp
+++ label_imgs/DualImageWindow.hpp
@@ -1,18 +1,15 @@
 #include <opencv2/core.hpp>
 #include <vector>
-using std::string;
-using std::vector;
-using std::pair;
-using cv::Mat;
-using cv::Point2i;
+
+typedef std::pair<cv::Point2i,cv::Point2i> PointPair;
 namespace imagelabeling 
 {
    class DualImageWindow 
    {
       public:
-         explicit DualImageWindow (const Mat left_img, const Mat right_img, 
-               const string window_name = "Label Images");
-         vector<pair<Point2i,Point2i>> points();
+         explicit DualImageWindow (const cv::Mat left_img, const cv::Mat right_img, std::vector<PointPair> initial_points = std::vector<PointPair>(),
+               const std::string window_name = "Label Images");
+         std::vector<std::pair<cv::Point2i,cv::Point2i>> points();
          void show();
 
 
@@ -20,14 +17,14 @@ namespace imagelabeling
          virtual ~DualImageWindow();
 
       private:
-         Mat left_img, right_img, combined_imgs;
-         string window_name;
-         vector<Point2i> correspondences;
+         cv::Mat left_img, right_img, combined_imgs;
+         std::string window_name;
+         std::vector<cv::Point2i> correspondences;
          bool firstPointSet;
 
          void refresh();
          bool handleKeyEvent(const int key);
-         void combine_imgs(const Mat& left_img, const Mat& right_img);
+         void combine_imgs(const cv::Mat& left_img, const cv::Mat& right_img);
    };
 
 } /* namespace imagelabeling */
